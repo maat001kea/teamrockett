71b68ef99936c6172ad7a781a00f74f6fdecafdf
Mahmoud Atie <maat0001@stud.kea.dk>
2025-05-08 11:38:28 +0200

    NYT

--- src/app/components/EventList.jsx
+++ src/app/components/EventList.jsx
@@ -3,4 +3,4 @@
 // når jeg poster skal jeg bare tænk på at smide data over,
 //hvis det virer, kan jeg så sætte et form op som jeg kan inpute info ind
 
-// VIGTIGT TJEK ALTID I THUNDER CLIENT INDEN DU LIGGER NOGET UPP
+// VIGTIGT TJEK ALTID I THUNDER CLIENT INDEN DU LIGGER NOGET UP
