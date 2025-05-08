7606a1c513edb38d6dec37de45c72cf940f89d77
Mahmoud Atie <maat0001@stud.kea.dk>
2025-05-08 11:27:10 +0200

    NYT

---
+++ src/app/components/EventList.jsx
@@ -0,0 +1,6 @@
+//mappe alle events i localhost
+// virker det så lave en knap der poster et event
+// når jeg poster skal jeg bare tænk på at smide data over,
+//hvis det virer, kan jeg så sætte et form op som jeg kan inpute info ind
+
+// VIGTIGT TJEK ALTID I THUNDER CLIENT INDEN DU LIGGER NOGET UP
---
+++ src/app/components/KunstListe.jsx
@@ -0,0 +1,5 @@
+//Kan jeg tag og lave et liste fra databasen smk
+// af f.eks 50 thing uden den crasher
+// vhsi jeg har en list kan jeg sorte den på et eller anden måde
+
+//VIGTIGSTE LAVE DE 2 ØVERSTE PÅ BEGGE SIDER
