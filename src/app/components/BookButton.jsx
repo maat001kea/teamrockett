9927b1fa8a84c556cd9e7e7c392e1ddc6698edde
Mahmoud Atie <maat0001@stud.kea.dk>
2025-05-13 12:11:49 +0200

    nyt

--- src/app/components/BookButton.jsx
+++ src/app/components/BookButton.jsx
@@ -14,7 +14,7 @@ export default function BookButton({ id }) {
       await bookEvent(id, { quantity: booked ? -1 : 1 });
 
       // Sikrer at spinner vises i mindst 1.5 sekunder
-      await new Promise((res) => setTimeout(res, 1500));
+      await new Promise((res) => setTimeout(res, 500));
 
       setBooked(!booked);
     } catch (err) {
