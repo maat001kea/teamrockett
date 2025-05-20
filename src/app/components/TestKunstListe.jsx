d6b646e13e495ec53be789015e760579c4d87a5b
Viktor <viho0002@stud.kea.dk>
2025-05-20 09:25:02 +0200

    search test

--- src/app/components/TestKunstListe.jsx
+++ src/app/components/TestKunstListe.jsx
@@ -1,10 +1,17 @@
-const TestKunstListe = async () => {
-  const data = await fetch("https://api.smk.dk/api/v1/art/search/?keys=*&offset=0&rows=100");
-  const kunst = await data.json();
+"use client";
+import { useRouter } from "next/navigation";
 
+const TestKunstListe = ({ data }) => {
+  const router = useRouter();
+
+  function searchThingy() {
+    const search = "bob";
+    router.push(`/testkunst?search=${search}`);
+  }
   return (
     <div>
-      {kunst.items.map((item) => {
+      <button onClick={searchThingy}>skift</button>
+      {data.items.map((item) => {
         return <li key={item.id}>{item.id}</li>;
       })}
     </div>
--- src/app/testkunst/page.js
+++ src/app/testkunst/page.js
@@ -1,5 +1,9 @@
 import TestKunstListe from "../components/TestKunstListe";
 
-export default function Page() {
-  return <TestKunstListe />;
+export default async function Page({ searchParams }) {
+  const { search } = await searchParams;
+
+  const data = search ? await fetch(`https://api.smk.dk/api/v1/art/search/?keys=${search}&offset=0&rows=100`) : await fetch(`https://api.smk.dk/api/v1/art/search/?keys=*&offset=0&rows=100`);
+  const kunst = await data.json();
+  return <TestKunstListe data={kunst} />;
 }
