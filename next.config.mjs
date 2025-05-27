553284bf04cde7d52f21042ecdeeb3699347e008
Mahmoud Atie <maat0001@stud.kea.dk>
2025-05-27 08:19:06 +0200

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.dummyjson.com", "iip-thumb.smk.dk", "api.smk.dk", "iip.smk.dk"],
+    domains: [
+      "cdn.dummyjson.com",
+      "iip-thumb.smk.dk",
+      "api.smk.dk",
+      "iip.smk.dk",
+      "laqizwqplonobdzjohhg.supabase.co", // Tilføjet Supabase domæne
+    ],
   },
   transpilePackages: ["@clerk/nextjs"],
 };
