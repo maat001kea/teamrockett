26c8835ea22ef38c83e4577a973ff5e2a0bd3461
Mahmoud Atie <maat0001@stud.kea.dk>
2025-05-29 10:18:27 +0200

    vercelclerk

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,7 +3,7 @@ export const dynamic = "force-dynamic";
 export default function NotFoundPage() {
   return (
     <div>
-      <h1>404 - Siden findes ikke</h1>
+      <h1>404 - Siden findes ikkke</h1>
       <p>Ups! Siden du leder efter, blev ikke fundet.</p>
     </div>
   );
